e1c1fd9cb1ac60cbc35ceac4ab527badb1b5aaca
sets018 <91557108+sets018@users.noreply.github.com>
2022-10-12 11:40:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -255,10 +255,11 @@ class graph():
     usr_a = self.inv_nodes_dict.get(usr_input_a)
     usr_b = self.inv_nodes_dict.get(usr_input_b)
     usr_pair = (usr_a,usr_b)
-    st.write(usr_pair)
+    st.write(self.inv_nodes_dict)
+    st.write('a', usr_input_a,usr_input_b)
+    st.write('b', usr_a,usr_b)
     # Basado en el punto de origen y de destinacion que da el usuario encuentra la ruta de camino minimo en el diccionario
     self.usr_path = self.paths_dict.get(usr_pair)
-    st.write(self.usr_path)
     cities = []
     for node in self.usr_path:
       city = self.inv_nodes_dict.get(node)
